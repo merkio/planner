3d0754c860766dc8e532ee0981dc02227307fec9
Merkushev <igor.merkushev@scalable.capital>
2022-08-28 18:40:52 +0200

    rm unused import

--- repository/src/test/kotlin/space/geek/planner/repository/RepositoryTestBase.kt
+++ repository/src/test/kotlin/space/geek/planner/repository/RepositoryTestBase.kt
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.PostgreSQLContainer
